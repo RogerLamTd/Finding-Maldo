f1ce231696cc76548fb6023d8e11c99b4cf23908
mzhang181 <mzhang181@gmail.com>
2021-01-30 14:39:52 -0500

    removed react

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,10 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#@app.before_first_request()
+#def getBaseImage():
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80)
