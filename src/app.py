9762717b3856732ec55017be5e96c9bffcd16e42
mzhang181 <mzhang181@gmail.com>
2021-01-30 21:30:30 -0500

    monkE

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import crop
+import base64
 
 app = Flask(__name__, static_url_path="")
 
@@ -17,9 +18,28 @@ app = Flask(__name__, static_url_path="")
 def login():
     url = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.gannett-cdn.com%2F-mm-%2F279e76e11f691d6589f96f1782362c72d2b182f4%2Fc%3D0-208-4096-2522%26r%3Dx1803%26c%3D3200x1800%2Flocal%2F-%2Fmedia%2F2015%2F01%2F11%2FUSATODAY%2FUSATODAY%2F635565865838731514-EPA-FRANCE-PARIS-SOLIDARITY-RALLY.jpg&f=1&nofb=1"
     baseReq = requests.get(url, stream = True)
-    baseImg = Image.open(BytesIO(baseReq.content))
-    maskImg, xOffset, yOffset = crop.getMask(baseImg)
-    return render_template('index.html', base=baseImg, mask=maskImg)
+   # baseReq.content.decode = True
+    baseBytes = BytesIO(baseReq.content)
+    img64 = base64.b64encode(baseBytes.getvalue())
+    print(baseReq.headers) 
+    print(type(baseReq.content))
+    #baseImg = Image.frombytes("RGB", len(baseReq.content),baseReq.content)
+    baseImg = Image.open(baseBytes)
+
+
+    maskImg, xOffset, yOffset, maskSize = crop.get_Mask(baseImg)
+    #xOffset = 2229
+    #yOffset = 815
+    #maskImg = Image.open("static/mask.png")
+   # maskSize = (50,50)
+    print(xOffset, yOffset)
+    print(maskSize)
+    
+    byte_buf = BytesIO()
+    maskImg.save(byte_buf, "PNG")
+    mask64 = base64.b64encode(byte_buf.getvalue())
+    return render_template('index.html', base=img64.decode('utf-8'), mask=mask64.decode('utf-8'), mask_x = "{}px".format(xOffset), mask_y = "{}px".format(yOffset),
+        mask_w = "{}px".format(maskSize[0]//2), mask_h = "{}px".format(maskSize[1]//2))
 
 
 if __name__ == '__main__':
--- src/crop.py
+++ src/crop.py
@@ -1,8 +1,13 @@
 from PIL import Image
 import random
+import os
+from io import BytesIO
 from google.cloud import vision
 from matplotlib import pyplot as plt 
 from matplotlib import patches as pch 
+
+
+os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 def crop_image(img):
     im_w, im_h = img.size
@@ -27,34 +32,37 @@ def crop_image(img):
     ret_img = img.crop((x0, y0, x1, y1))
     return [ret_img, x0, y0]
 
-def pick_face(content):
+def pick_face(crop):
     
     face_coords = []
     
-    image = vision.Image(content=content)
+    img_byte_arr = BytesIO()
+    crop.save(img_byte_arr, format='JPEG')
+    image = vision.Image(content=img_byte_arr.getvalue())
+
+    client = vision.ImageAnnotatorClient()
 
     response = client.face_detection(image=image, max_results=50)
     faces = response.face_annotations
     
-    a = plt.imread(path)
+    a = plt.imread(img_byte_arr, format = "JPEG")
     fig, ax = plt.subplots(1)
     ax.imshow(a)
     
-
     for face in faces:
         vertices = ([(vertex.x, vertex.y)
                     for vertex in face.bounding_poly.vertices])
         
         face_coords.append(vertices)
         
-        #print('face bounds: ', vertices)
-        # vertices are as follows: top left, top right, bottom right, bottom left.
+        print('face bounds: ', vertices)
+        #vertices are as follows: top left, top right, bottom right, bottom left.
     
-        #rect = pch.Rectangle(vertices[0], (vertices[1][0] -  vertices[0][0]), 
-        #                (vertices[2][1] - vertices[0][1]), linewidth = 1, edgecolor ='r', facecolor ='none') 
-        # pch.Rectangle( (26, 453), 20, 23)
+        rect = pch.Rectangle(vertices[0], (vertices[1][0] -  vertices[0][0]), 
+                        (vertices[2][1] - vertices[0][1]), linewidth = 1, edgecolor ='r', facecolor ='none') 
+        pch.Rectangle( (26, 453), 20, 23)
         
-        #ax.add_patch(rect)
+        ax.add_patch(rect)
 
     if response.error.message:
         raise Exception(
@@ -62,21 +70,40 @@ def pick_face(content):
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
     
-    randint = int((len(faces)*random.random())
+    randint = int((len(faces)*random.random()))
     chosen_face = face_coords[randint]
     rect = pch.Rectangle(chosen_face[0], (vertices[1][0] -  vertices[0][0]), 
                         (vertices[2][1] - vertices[0][1]), linewidth = 1, edgecolor ='r', facecolor ='none')
     ax.add_patch(rect)
+    fig.savefig("plot.png")
         
     return chosen_face
 # return type is a list of lists of tuples representing where each internal list represents a face, and each
 # tuple is a coordinate of the face; top left, top right, bottom right, bottom left respectively.
 
-def makeMask(face):
-
+def make_Mask(face):
+    mask = Image.open("static/mask.png")
+    print(face[1][0] - face[0][0], face[3][1] - face[0][1])
+    mask = mask.resize((face[1][0] - face[0][0], (face[3][1] - face[0][1])// 2))
+    return (mask, (mask.width, mask.height))
 
 def get_Mask(base):
     tagImage, cropX, cropY = crop_image(base)
     faceBox = pick_face(tagImage)
-    maskImg = makeMask(face)
-    return (maskImg, cropX + faceBox[0][0], cropY + faceBox[0][1])
+    maskImg, maskSize = make_Mask(faceBox)
+    return (maskImg, cropX + faceBox[0][0] + maskSize[0]//2, cropY + faceBox[0][1], maskSize)
+
+
+#def get_Bernie(base):
+    #tagImage, cropX, cropY = crop_image(base)
+    #faceBox = pick_face(tagImage)
+    #maskImg, maskSize = make_Mask(faceBox)
+#
+#
+    #mask = Image.open("static/bernie_head.png")
+    #print(face[1][0] - face[0][0], face[3][1] - face[0][1])
+    #mask = mask.resize((face[1][0] - face[0][0], (face[3][1] - face[0][1])// 2))
+    #return (mask, (mask.width, mask.height))
+#
+#    
+    #return (maskImg, cropX + faceBox[0][0] + maskSize[0]//2, cropY + faceBox[0][1], maskSize)
